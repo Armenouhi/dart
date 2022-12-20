b35c523b6c7deac374b2a75fa8c278b6eb104bcf
Armenouhi <40177133+Armenouhi@users.noreply.github.com>
2022-12-20 16:34:27 +0400

    the bug fixed

--- ex33.dart
+++ ex33.dart
@@ -2,58 +2,49 @@ void main(List<String> args) {
   List<int> list = [15, 26, 7, 4, 47, 11, 91, 19, 45, 74, 18];
 
   print(mergeSort(list));
-  // mergeSort(list);
 }
 
 List<int> mergeSort(List<int> list) {
   int length = list.length;
+  int mid = (length / 2).round();
 
-  List<int> left = [], right = [];
-
-  if (length > 1) {
-    left = List.generate((length / 2).round(), (index) => list[index]);
-    right = List.generate(
-      length % 2 == 0 ? (length / 2).round() : ((length / 2).round() - 1),
-      (index) => list.reversed.toList()[index],
-    );
-
-    print(mergeSort(right));
-
-    return merge(mergeSort(left), mergeSort(right));
-  } else {
+  if (list.length <= 1) {
     return list;
   }
+
+  List<int> left = [], right = [];
+  left = List.generate(mid, (index) => list[index]);
+  // print(left);
+  right = List.generate(length % 2 == 0 ? mid : mid - 1,
+      (index) => list.reversed.toList()[index]);
+  // print(right);
+
+  return merge(mergeSort(left), mergeSort(right));
 }
 
 merge(List<int> left, List<int> right) {
   int i = 0, j = 0;
-
-  List<int> list = [];
+  List<int> result = [];
 
   while (i < left.length && j < right.length) {
-    if (left[i] < right[j]) {
-      list.add(left[i]);
+    if (left[i] <= right[j]) {
+      result.add(left[i]);
       i++;
-    } else if (left[i] > right[j]) {
-      list.add(right[j]);
-      j++;
     } else {
-      list.add(left[i]);
-      i++;
-      list.add(right[j]);
+      result.add(right[j]);
       j++;
     }
   }
 
-  while (i < left.length - 1) {
-    list.add(left[i]);
+  while (i < left.length) {
+    result.add(left[i]);
     i++;
   }
 
   while (j < right.length) {
-    list.add(right[j]);
+    result.add(right[j]);
     j++;
   }
 
-  return list;
+  return result;
 }
